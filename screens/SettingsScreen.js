e974e29ab750d3fbd0ce2aa6d312e98ff75a3351
Steven Monroe <smonroe@e1z3r1p20.42.us.org>
2019-04-08 00:34:14 -0700

    docker deploying

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -20,7 +20,7 @@ export default class SettingsScreen extends React.Component {
            <Text style={styles.settingsText}>
             [Insert Preference switches here]
            </Text>
-           <Switch onValueChange = {console.log("Switch it up")}/>
+           <Switch onValueChange = {value ^= true}/>
          </View>
         </ScrollView>
     </View>
