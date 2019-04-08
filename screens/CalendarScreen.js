92fe9a6c174eb095c76e2ade9cd5765b008d9eef
Steven Monroe <smonroe@e1z3r1p20.42.us.org>
2019-04-08 01:50:48 -0700

    docker deploying

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -16,6 +16,11 @@ export default class CalendarScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Calendar layout */}
         <Text>Calendar will end up here one day... but for now as a test... if we make long strings... what happens?</Text>
+        <View style={{ flex: 1, flexDirection: "row", alignSelf: "stretch" }}>
+	        <View style={{ flex: 1, alignSelf: 'stretch' }} />
+	        <View style={{ flex: 1, alignSelf: 'stretch' }} />
+	        <View style={{ flex: 1, alignSelf: 'stretch' }} />
+        </View>
       </ScrollView>
     );
   }
