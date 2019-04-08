a2596c0cb0b78b036352d26791294f07ff538da7
Steven Monroe <smonroe@e1z3r1p20.42.us.org>
2019-04-07 20:28:31 -0700

    Test of user frontend changes

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+//import { ExpoCalendarView } from '@expo/samples';
 
-export default class LinksScreen extends React.Component {
+export default class CalendarScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Calendar',
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        {/* Calendar layout */}
+        <Text>Calendar will end up here one day...</Text>
       </ScrollView>
     );
   }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,69 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Preferences',
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container}>
+         <View style={styles.container}>
+           <Text style={style.settingsText}>
+           ?!🍣🍕🌮🍮🍻🍦!?
+           whatchu want fool
+           </Text>
+         </View>
+
+         <View style={styles.container}>
+           <Text style={styles.settingsText}>
+             Select your preferences!!!
+           </Text>
+          </View>
+        </ScrollView>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.settingsText}>What are we putting in this bottom bar, anything?</Text>
+      </View>
+    </View>
+
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  settingsText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  }
+})
