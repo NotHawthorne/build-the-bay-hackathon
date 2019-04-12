4f8152e5f606f28fb8859d9119c3806bac2e53c9
alex kozma <nothawthorne@airmail.cc>
2019-04-12 14:00:51 +0000

    changed shit

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -5,6 +5,6 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
   resolver: {
-    blacklistRE: blacklist([/awsmobilejs\/#current-backend-info\/.*/])
+    blacklistRE: blacklist([/awsmobilejs\/.*/])
   }
 };
